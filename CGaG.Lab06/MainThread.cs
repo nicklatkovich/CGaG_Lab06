5e3cc1c91a9956c17beb90611fda557df3dee258
NickLatkovich <nicklatkovich@gmail.com>
2017-05-07 17:14:05 +0300

    Init

---
+++ CGaG.Lab06/MainThread.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CGaG.Lab06 {
+    public class MainThread : Game {
+        GraphicsDeviceManager Graphics;
+        SpriteBatch SpriteBatch;
+        Color BackColor = new Color(30, 30, 30);
+
+        public MainThread( ) {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize( ) {
+            // TODO: Add initialization logic
+
+            base.Initialize( );
+        }
+
+        protected override void LoadContent( ) {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load content
+        }
+
+        protected override void UnloadContent( ) {
+            // TODO: Unload any non ContentManager content
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState( ).IsKeyDown(Keys.Escape))
+                Exit( );
+
+            // TODO: Add update logic
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(BackColor);
+
+            // TODO: Add drawing code
+
+            base.Draw(gameTime);
+        }
+    }
+}
